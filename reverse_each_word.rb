fd082baa2e6837ee38abd92b9ae1ec714005b098
Joseph Guillory <jagmanx99@yahoo.com>
2022-07-07 23:50:48 -0700

    complete

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,15 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    reversed_words = words.map do |word|
+        word.reverse
+    end
+
+    reversed_words.join(" ")
+end
+
+binding.pry
+0
